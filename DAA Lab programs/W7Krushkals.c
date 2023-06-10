22cdd1ca2c1a266349d12049b689f89485192658
KLN <95397474+KLN-loki@users.noreply.github.com>
2023-06-10 12:02:17 +0530

    Update W7Krushkals.c

--- DAA Lab programs/W7Krushkals.c
+++ DAA Lab programs/W7Krushkals.c
@@ -51,12 +51,11 @@ int main() {
     printf("\nImplementation of Kruskal's algorithm\n");
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the cost adjacency matrix:\n");
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    printf("Enter the cost adjacency matrix (-1 for infinity):\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0)
-                cost[i][j] = INT_MAX;
+            if(cost[i][j] == -1) cost[i][j] = INT_MAX;
         }
     }
     kruskalsAlgorithm(n);
